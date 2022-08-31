b2ab98c392237193fbd2017109c224f8686d3874
feng-linpeng <feng-linpeng@users.noreply.github.com>
2022-08-31 09:14:16 +0000

    :memo: ts函数方法更新

---
+++ assets/views_css_index.md.f9185233.js
@@ -0,0 +1,1 @@
+import{_ as e,c as s,o as t}from"./app.dad300e2.js";const m=JSON.parse('{"title":"css\u65B9\u6CD5","description":"","frontmatter":{"title":"css\u65B9\u6CD5"},"headers":[],"relativePath":"views/css/index.md"}'),a={name:"views/css/index.md"};function c(n,r,o,i,d,p){return t(),s("div")}const f=e(a,[["render",c]]);export{m as __pageData,f as default};
